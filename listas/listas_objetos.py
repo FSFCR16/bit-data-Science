9da64a7d18c4558a8cea0d14499916d36ac1a8a4
FSFCR16 <fajarfsabo161@gmail.com>
2024-03-06 22:18:01 -0500

    update

--- listas/listas_objetos.py
+++ listas/listas_objetos.py
@@ -2,9 +2,20 @@
 
 lista = [1,2,3,4,5,6]
 
+# Otra forma de crear una lista es con el objeto list, que recibe como argumentos los elementos que va a tener la lista
+
+### EJEMPLO ###
+
+new_list = list((2, 3, True, "Juan" ))
+
+print(new_list)
+
+#### LOG ####
+# [2, 3, True, 'Juan']
+
 # Metodo append
 
-# Este metodo sirve para agregar elemntos a lista estos elemntos seran agragados al final de la lista
+# Este metodo sirve para agregar elemntos a lista, estos elementos seran agragados al final de la lista
 
 lista.append(3)
 print(lista)
@@ -82,3 +93,83 @@ print(lista)
 lista.pop()
 
 print(lista)
+
+# Listas anidadas, quiere decir que hay listas dentro de listas 
+
+### EJEMPLO ###
+
+lista_anidada = [["juan", False], [2, 5], [64, 32]]
+
+# Si se desea acceder al un elemento de una lista anidada una forma seria
+
+print(lista_anidada[0][1])
+
+#### LOG ####
+
+# False
+
+# Tambien aparte de hacer slicing con los indices podemos hacer que vaya de 2 en 2 o de 3 en 3
+
+### EJEMPLO ###
+
+# slicing
+
+lista_slice = [2,4,5,6,7,8]
+
+print(lista_slice[:3]) # aca le estamos diciendo que vaya desde el inicio hasta la posicion 3, no toma la posicion 3.
+
+#### LOG ####
+
+#[2,3,5]
+
+# Ahora que vaya de 2 en dos
+
+print(lista_slice[0:5:2])
+
+#### LOG #### 
+
+# [2, 5, 7]
+
+# Otra forma para dar el sentido inverso sin usar el reverse es la siguiente
+
+print(lista_slice[::-1]) # Esto no estoy seguro, estoy supuniendo pero supongo que se debe a que 
+# si tu dices que vas a ir de -1 en -1 se va ir sumando -1 sera -2 depues -3 y asi cambiando el orden de la lista
+
+
+# Tambien podemos multiplicar la lista y sumar que en realidad en la caso de la multiplicacion seria poner en una lista los elementos de la lista base
+# la cantidad de n entonces si n es 3 la lista base estara en lista unica 3 veces y en el caso de la suma funciona como concatenacion normal
+
+### EJEMPLO ###
+ 
+print(lista_slice*3)
+
+#### LOG ####
+
+# [2, 4, 5, 6, 7, 8, 2, 4, 5, 6, 7, 8, 2, 4, 5, 6, 7, 8]
+
+# Con la suma
+
+print(lista_slice + lista_dos)
+
+#### LOG ####
+
+# [2, 4, 5, 6, 7, 8, 5, 6, 7, 8, 9]
+
+# Tambien podemos simular el funcionamiento del metodo "split" seria un split que no se le pasa nada como argumento 
+# con el objeto list, pasandole como argumento un str y el covierte cada caracter deñ str en 
+# un elemento de la lista
+
+print(list("hola soy santiago fajardo"))
+
+#### LOG ####
+
+# ['h', 'o', 'l', 'a', ' ', 's', 'o', 'y', ' ', 's', 'a', 'n', 't', 'i', 'a', 'g', 'o', ' ', 'f', 'a', 'j', 'a', 'r', 'd', 'o']
+
+# Una forma de desestructurar una lista seria esta
+
+num, *numotros = lista_slice
+
+print(num) #### LOG #### 2
+print(numotros) #### LOG #### 4 5 6 7 8
+
+print(dir(list()))
--- variables 2.0/tupla.py
+++ variables 2.0/tupla.py
@@ -4,4 +4,17 @@ tupla = tuple(['hola', 2121312])
 
 tuplaDos = "hola", "la"
 
+# nesting listas dentro de tuplas
+tpl=([1,2,3],['David','Pedro',True])
+print(tpl)
+tpl[0].append('Tuy')
+print(tpl)
+tpl[1].remove('David')
+print(tpl)
+
+#### LOGS ####
+([1, 2, 3], ['David', 'Pedro', True])
+([1, 2, 3, 'Tuy'], ['David', 'Pedro', True])
+([1, 2, 3, 'Tuy'], ['Pedro', True])
+
 print(tupla, tuplaDos)
